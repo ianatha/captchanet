cf5531c236866b934e75ba11358146af5c86d8e4
Ian Atha <ian@atha.io>
2019-10-13 18:00:41 -0700

    listen on 0.0.0.0 instead of localhost by default

--- guesssrv.py
+++ guesssrv.py
@@ -73,7 +73,7 @@ class RestfulCaptchanetServer():
         parser.add_argument(
             "-l",
             "--listen",
-            default="localhost",
+            default="0.0.0.0",
             help="Specify the IP address on which the server listens",
         )
 
